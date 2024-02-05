6a7ca18542eb80694dc69ec3591e11f62579f901
Erik Kristensen <erik@erikkristensen.com>
2024-02-04 17:21:54 -0700

    cleanup

--- Formula/aws-nuke.rb
+++ Formula/aws-nuke.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class AwsNuke < Formula
   desc "Remove all the resources from an AWS account. "
   homepage "https://ekristen.github.io/aws-nuke/"
