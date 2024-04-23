f160101747a7546e888561510c123f7b76d3e325
Erik Kristensen <erik@erikkristensen.com>
2024-04-22 18:08:29 -0600

    Update azure-nuke@1.rb

--- Formula/azure-nuke@1.rb
+++ Formula/azure-nuke@1.rb
@@ -4,16 +4,8 @@ class AzureNukeAT1 < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/ekristen/azure-nuke/releases/download/v1.0.0-next.11/azure-nuke-v1.0.0-next.11-darwin-amd64.tar.gz"
-      sha256 "e591c54c4a77cdea860dccf25f9ceab770fa68c9e4085030b78fd839c4b5737c"
-
-      def install
-        bin.install "azure-nuke"
-      end
-    end
-    if Hardware::CPU.arm?
-      url "https://github.com/ekristen/azure-nuke/releases/download/v1.0.0-next.4/azure-nuke-v1.0.0-next.4-darwin-arm64.tar.gz"
-      sha256 "5e63fa435b926d5935b414bebd542a375eb632241315e86fa1a1ebfa4414ae76"
+      url "https://github.com/ekristen/azure-nuke/releases/download/v1.0.0-beta.3/azure-nuke-v1.0.0-beta.3-darwin-amd64.tar.gz"
+      sha256 "9facf7c068a365adc6641f8cdaea30b6e3b09af43f0594a23956b437dffd6259"
 
       def install
         bin.install "azure-nuke"
@@ -23,24 +15,8 @@ class AzureNukeAT1 < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/ekristen/azure-nuke/releases/download/v1.0.0-next.11/azure-nuke-v1.0.0-next.11-linux-amd64.tar.gz"
-      sha256 "a558899ffd1b6256e04b1da81ed51778989a806897d1b8f2869336706ed342c7"
-
-      def install
-        bin.install "azure-nuke"
-      end
-    end
-    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/ekristen/azure-nuke/releases/download/v1.0.0-next.4/azure-nuke-v1.0.0-next.4-linux-arm7.tar.gz"
-      sha256 "5c33bf3521b185e3002ec0a5f5285ef03a451591d9a56eddfdd81b795d55485e"
-
-      def install
-        bin.install "azure-nuke"
-      end
-    end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/ekristen/azure-nuke/releases/download/v1.0.0-next.4/azure-nuke-v1.0.0-next.4-linux-arm64.tar.gz"
-      sha256 "01e40b445924897cec8fc66838b0c24b4ec8578ee7413f283ef7f65944520124"
+      url "https://github.com/ekristen/azure-nuke/releases/download/v1.0.0-beta.3/azure-nuke-v1.0.0-beta.3-linux-amd64.tar.gz"
+      sha256 "beb14c78f5343f12f413a1ee35858f07750a97f50beaa31fa8cda9f91cf43b70"
 
       def install
         bin.install "azure-nuke"
